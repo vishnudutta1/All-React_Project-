5b6aa07df021f6de183e9822e73538d1323bcaf6
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-04-07 23:23:56 +0530

    react

---
+++ tik_tak/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <p className='Hading'>TIC TAC TOE</p>
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tik_tak/src/components/Square_box.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+function Square_box(){
+
+    return (
+
+    )
+}
